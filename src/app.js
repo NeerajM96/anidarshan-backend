9263c560a8fe0cf419c3379262120b96117edeb8
Neeraj <neerajmilan968@gmail.com>
2024-01-09 15:53:20 +0530

    added files in utils

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// configurations
+// for data coming in form of json
+app.use(express.json({limit: "16kb"}))
+
+// for data coming from url eg. %20 used for empty space in URL and we need to tell our server what this %20 means etc.
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+// In case we need to some some static data like images on our server
+app.use(express.static("public"))
+
+// to perform crud operations on cookies stored on client's browser as only server can read and remove client's cookies from their browser
+app.use(cookieParser())
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 // dotenv has always be the first to import as it is needed right away
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: "./env"
 })
 
+const PORT = process.env.PORT || 8000
+
+// async-await(used in db connection file) returns a promise
 connectDB()
+.then(
+    app.listen(PORT, ()=>{
+        console.log(`Server is running at port: ${PORT}`)
+    })
+)
+.catch(error=>{
+    console.log("MongoDB Connection Failed !!! ",error)
+})
